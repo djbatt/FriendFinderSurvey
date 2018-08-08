5bfbe80771f270ba656c8b4e5b57cf3d1744f70c
djbatt <djbatt@live.com>
2018-08-08 13:52:49 -0400

    couldn't get it working yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,4 +2,40 @@ const path = require("path");
 
 var friends = require("../data/friends.js");
 
-var totalDifference = 0;
+module.exports = function(app) {
+    app.get("/friends", function(req, res) {
+        res.json(friends);
+    })
+
+    app.post("/friends", function(req, res) {
+
+        var input = req.body;
+
+        var compareResponses = input.scores;
+
+        var matchName = "";
+        var matchImg = "";
+
+        var difference = 1000;
+
+        for (var i = 0; i < friend.length; i++) {
+
+            var lowDifference = 0;
+            
+            for (var j = 0; j < compareResponses.length; j++) {
+                lowDifference += Math.abs(friends[i].scores[j] - compareResponses[i]);
+            }
+
+            if (lowDifference < difference) {
+                difference = lowDifference;
+                matchName = friends[i].name;
+                matchImg = friend[i].photo;
+            }
+        }
+
+        friends.push(input);
+
+        res.json({status: 200, matchName: matchName, matchImg: matchImg});
+
+    })
+}
--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require("express");
 
 const path = require("path");
 
-var bodyParser = require("body-parser")
-
 var app = express();
 var port = process.env.port || 3000;
 
 app.use(express.static(path.join(__dirname, "./app/public")))
 
+var bodyParser = require("body-parser")
+
+require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
-require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
 
 app.listen(port, function() {
     console.log(`App listening on port: ${port}`);
