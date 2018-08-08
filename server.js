516394a066dc027b5d55b9344e7acd7ae25fdd89
djbatt <djbatt@live.com>
2018-08-08 12:00:52 -0400

    added html routes, and updated server to connent

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ var express = require("express");
 const path = require("path");
 
 var bodyParser = require("body-parser")
+
+var app = express();
+var port = process.env.port || 3000;
+
+app.use(express.static(path.join(__dirname, "./app/public")))
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes.js");
+require("./app/routing/htmlRoutes.js");
+
+app.listen(port, function() {
+    console.log(`App listening on port: ${port}`);
+})
